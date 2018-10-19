f6d1f6e556b63b789471881f0a6d1e490de9daf8
Mateusz Konieczny <matkoniecz@gmail.com>
2018-10-19 15:38:24 +0200

    correctly specifu dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="osm_bot_abstraction_layer",
-    version="0.0.1",
+    version="0.0.2",
     author="Mateusz Konieczny",
     author_email="matkoniecz@gmail.com",
     description="A tool for easier automation of OSM edits without causing problems.",
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/matkoniecz/osm_bot_abstraction_layer",
     packages=setuptools.find_packages(),
+    install_requires = [
+        'osmapi>=1.0.2, <2.0',
+        'termcolor>=1.1.0, <2.0',
+        'urllib3>=1.13.1, <2.0',
+        'osm_iterator>=1.0.0, <2.0',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
