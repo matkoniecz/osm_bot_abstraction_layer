6f97f0cd3a5a1dbf26c455dd7d76f330c5685c99
Mateusz Konieczny <matkoniecz@gmail.com>
2020-11-26 13:43:44 +0100

    refactor and split

--- osm_bot_abstraction_layer/world_data.py
+++ osm_bot_abstraction_layer/world_data.py
@@ -13,11 +13,8 @@ class NameCollector( object ):
     def collected_names (self):
       return self.names
 
-def list_of_area_divisions(area_iso_code, admin_level_of_split, temporary_storage_file):
-    """returns list with names of subdivisions of area with specific iso code
-    (for example PL for Poland), at specified aministrative level"""
-    for iso_tag in ["ISO3166-1", "ISO3166-1:alpha2", "ISO3166-2"]:
-      internal_division_query = """[out:xml][timeout:3600];
+def list_of_area_divisions_query(iso_tag, area_iso_code, admin_level_of_split):
+    return """[out:xml][timeout:3600];
   area
     ['""" + iso_tag + """'='""" + area_iso_code + """'] -> .parent_area;
   (
@@ -26,6 +23,12 @@ def list_of_area_divisions(area_iso_code, admin_level_of_split, temporary_storag
       [admin_level=""" + str(admin_level_of_split) + """][boundary=administrative];
   );
   out tags;"""
+
+def list_of_area_divisions(area_iso_code, admin_level_of_split, temporary_storage_file):
+    """returns list with names of subdivisions of area with specific iso code
+    (for example PL for Poland), at specified aministrative level"""
+    for iso_tag in ["ISO3166-1", "ISO3166-1:alpha2", "ISO3166-2"]:
+      internal_division_query = list_of_area_divisions_query(iso_tag, area_iso_code, admin_level_of_split)
       overpass_downloader.download_overpass_query(internal_division_query, temporary_storage_file)
       osm = Data(temporary_storage_file)
       names = NameCollector()
