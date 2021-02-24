0fac6c278427e7176864a1b850f6b46c101ff9fb
Mateusz Konieczny <matkoniecz@gmail.com>
2021-02-24 09:35:29 +0100

    more logical function split

--- osm_bot_abstraction_layer/overpass_downloader.py
+++ osm_bot_abstraction_layer/overpass_downloader.py
@@ -8,24 +8,19 @@ def sleep(time_in_s):
         time.sleep(0.1)
 
 def download_overpass_query(query, filepath):
-    query = urllib.parse.quote(query)
     # see https://github.com/westnordost/StreetComplete/blob/6740a0b03996b929f9cf74ddb0e6afac7e3fc48e/app/src/main/res/xml/preferences.xml#L99
     faster_server ="https://lz4.overpass-api.de/api/interpreter"
     alt_server = "http://z.overpass-api.de/api/interpreter"
     server = random.choice([faster_server, alt_server])
-    url = server +"?data=" + query
     with open(filepath, 'w+') as file:
-        file.write(overpass_download(url))
+        file.write(get_reponse_from_overpass_server(server, query))
 
-def overpass_download(url):
+def get_reponse_from_overpass_server(api_url, query):
     print("sleeping before download")
     sleep(20)
     while True:
         try:
-            print("downloading " + url)
-            request = urllib.request.Request(url,headers={'User-Agent': 'overpass downloader for OSM bot'})
-            response = urllib.request.urlopen(request, timeout=25000)
-            print("succeded with", response.getcode(), "http code")
+            response = single_query_run(api_url, query)
             return response.read().decode('utf-8')
         except urllib.error.HTTPError as e:
             print(e.getcode())
@@ -40,3 +35,12 @@ def overpass_download(url):
             print(e)
             return
     print("overpass query failed!")
+
+def single_query_run(api_url, query):
+    query = urllib.parse.quote(query)
+    url = api_url +"?data=" + query
+    print("downloading " + url)
+    request = urllib.request.Request(url,headers={'User-Agent': 'overpass downloader for OSM bot'})
+    response = urllib.request.urlopen(request, timeout=25000)
+    print("succeded with", response.getcode(), "http code")
+    return response
