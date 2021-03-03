c02abff26d5fae0d5bee3cc6954e5458b1f52daa
Mateusz Konieczny <matkoniecz@gmail.com>
2021-03-03 13:33:08 +0100

    allow to specify timeout

--- osm_bot_abstraction_layer/overpass_downloader.py
+++ osm_bot_abstraction_layer/overpass_downloader.py
@@ -8,20 +8,20 @@ def sleep(time_in_s):
     for i in tqdm(range(time_in_s*10), ascii=True):
         time.sleep(0.1)
 
-def download_overpass_query(query, filepath):
+def download_overpass_query(query, filepath, timeout=1500):
     # see https://github.com/westnordost/StreetComplete/blob/6740a0b03996b929f9cf74ddb0e6afac7e3fc48e/app/src/main/res/xml/preferences.xml#L99
     faster_server ="https://lz4.overpass-api.de/api/interpreter"
     alt_server = "http://z.overpass-api.de/api/interpreter"
     server = random.choice([faster_server, alt_server])
     with open(filepath, 'w+') as file:
-        file.write(get_response_from_overpass_server(server, query))
+        file.write(get_response_from_overpass_server(server, query, timeout))
 
-def get_response_from_overpass_server(api_url, query):
+def get_response_from_overpass_server(api_url, query, timeout):
     #print("sleeping before download")
     #sleep(20)
     while True:
         try:
-            response = single_query_run(api_url, query)
+            response = single_query_run(api_url, query, timeout)
             return response.content.decode('utf-8')
         except requests.exceptions.HTTPError as e:
             print(e.response.status_code)
@@ -33,12 +33,12 @@ def get_response_from_overpass_server(api_url, query):
             raise e
     print("overpass query failed!")
 
-def single_query_run(api_url, query):
+def single_query_run(api_url, query, timeout):
     print("downloading " + query)
     response = requests.post(
         api_url,
         data={'data': query},
-        timeout=25000,
+        timeout=timeout,
         headers={'User-Agent': 'overpass downloader for OSM bot'} #{"Accept-Charset": "utf-8;q=0.7,*;q=0.7"},
     )
     print("succeded with", response.status_code, "http code")
