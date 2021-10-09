a07dd7c116ce82c8aa2109a98f7bdbff2f5c6728
Mateusz Konieczny <matkoniecz@gmail.com>
2021-10-09 07:35:45 +0200

    relicense to AGPL
    https://opensource.stackexchange.com/questions/1723/can-i-release-under-agpl-a-program-that-uses-a-gpl-library

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
 ) 
