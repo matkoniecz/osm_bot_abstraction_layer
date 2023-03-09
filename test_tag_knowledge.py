3a82fca01da8557cb23c5f1dd75d9f237b13d26c
Mateusz Konieczny <matkoniecz@gmail.com>
2023-03-09 09:23:45 +0100

    extract preset tag data into a separate file

--- test_tag_knowledge.py
+++ test_tag_knowledge.py
@@ -1,24 +1,24 @@
 import unittest
-import osm_bot_abstraction_layer.human_verification_mode as human_verification_mode
+import osm_bot_abstraction_layer.tag_knowledge as tag_knowledge
 
 class Tests(unittest.TestCase):
     def test_empty(self):
         pass
 
     def test_nonsense_is_not_considered_as_valid_tag(self):
-        self.assertEqual(False, human_verification_mode.is_expected_tag("key", "value", {"key": "value"}, {}))
+        self.assertEqual(False, tag_knowledge.is_expected_tag("key", "value", {"key": "value"}, {}))
 
     def test_motels_may_be_in_hotel_buildings(self):
         tags = {"building": "hotel", "tourism": "motel"}
-        self.assertEqual(True, human_verification_mode.is_expected_tag("building", "hotel", tags, {}))
+        self.assertEqual(True, tag_knowledge.is_expected_tag("building", "hotel", tags, {}))
 
     def test_roads_may_have_bridges(self):
         tags = {"highway": "tertiary", "bridge": "yes"}
-        self.assertEqual(True, human_verification_mode.is_expected_tag("bridge", "yes", tags, {}))
+        self.assertEqual(True, tag_knowledge.is_expected_tag("bridge", "yes", tags, {}))
 
     def test_roads_may_have_lit_bridges(self):
         tags = {"highway": "tertiary", "bridge": "yes", "lit": "yes"}
-        self.assertEqual(True, human_verification_mode.is_expected_tag("lit", "yes", tags, {}))
+        self.assertEqual(True, tag_knowledge.is_expected_tag("lit", "yes", tags, {}))
 
 if __name__ == '__main__':
     unittest.main()
