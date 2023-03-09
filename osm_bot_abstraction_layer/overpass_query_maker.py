f33266981dd65dea0ea46a4f8400264343821dff
Mateusz Konieczny <matkoniecz@gmail.com>
2023-03-09 09:25:02 +0100

    make date format string more reusable

--- osm_bot_abstraction_layer/overpass_query_maker.py
+++ osm_bot_abstraction_layer/overpass_query_maker.py
@@ -1,2 +1,4 @@
+import utils
+
 def datetime_to_overpass_data_format(datetime_data):
-    return datetime_data.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return datetime_data.strftime(utils.typical_osm_timestamp_format())
---
+++ osm_bot_abstraction_layer/utils.py
@@ -0,0 +1,3 @@
+def typical_osm_timestamp_format():
+    # in Overpass and in OSM Editing API timestamps
+    return "%Y-%m-%dT%H:%M:%SZ"
