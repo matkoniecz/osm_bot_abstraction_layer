758d45ebbd1e929e80a0c9c27af9f79df2191d65
Mateusz Konieczny <matkoniecz@gmail.com>
2023-03-11 16:23:20 +0100

    add command specific to my system - but I am sole user of that code anyway, so...

--- reinstall.sh
+++ reinstall.sh
@@ -3,6 +3,8 @@ python3 setup.py sdist bdist_wheel
 cd dist
 pip3 uninstall osm_bot_abstraction_layer -y
 pip3 install --user *.whl
+pip3.10 uninstall osm_bot_abstraction_layer -y
+pip3.10 install --user *.whl
 cd ..
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
