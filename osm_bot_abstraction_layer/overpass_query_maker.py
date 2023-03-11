ea5e86a1f4c4c61f9f64c5cebc073d7e7095bbe2
Mateusz Konieczny <matkoniecz@gmail.com>
2023-03-11 10:50:02 +0100

    fix import

--- osm_bot_abstraction_layer/overpass_query_maker.py
+++ osm_bot_abstraction_layer/overpass_query_maker.py
@@ -1,4 +1,4 @@
-import utils
+import osm_bot_abstraction_layer.utils as utils
 
 def datetime_to_overpass_data_format(datetime_data):
     return datetime_data.strftime(utils.typical_osm_timestamp_format())
