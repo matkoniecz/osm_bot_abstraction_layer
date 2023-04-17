73005f8b309ec9a5963475dc3ec4822a4438ff13
Mateusz Konieczny <matkoniecz@gmail.com>
2023-04-17 23:51:45 +0200

    escape_overpass_value

--- osm_bot_abstraction_layer/overpass_query_maker.py
+++ osm_bot_abstraction_layer/overpass_query_maker.py
@@ -2,3 +2,6 @@ import osm_bot_abstraction_layer.utils as utils
 
 def datetime_to_overpass_date_format(datetime_data):
     return datetime_data.strftime(utils.typical_osm_timestamp_format())
+
+def escape_overpass_value(value):
+    return "'" + value.replace("\\", "\\\\") + "'"
