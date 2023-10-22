a2e94e6b73d9ef5143a1576c554f0fa0ce1e1eca
Mateusz Konieczny <matkoniecz@gmail.com>
2023-10-22 22:11:31 +0200

    better PL subdivision checker

--- test_world_data.py
+++ test_world_data.py
@@ -5,11 +5,11 @@ class Tests(unittest.TestCase):
     def test_voivodeship_count_in_poland(self):
         data = world_data.list_of_area_divisions_data("PL", 4, "name", "/tmp/test_file_PL_4.osm")
         #print(data)
-        #print(len(data))
+        if len(data) != 16:
+            print(world_data.list_of_area_divisions_query("ISO3166-1", "PL", 4))
 
         # for debug: is there a single object with a correct tags?
         # http://overpass-turbo.eu/s/10Ab
-        # ISO tags on https://www.openstreetmap.org/relation/936128 are wrong
 
         self.assertEqual(len(data), 16) # https://pl.wikipedia.org/wiki/Wojew%C3%B3dztwo 
         
