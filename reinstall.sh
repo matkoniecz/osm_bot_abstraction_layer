6181e1922fbbdd0cb969c026378b7b933bb9bf4a
Mateusz Konieczny <matkoniecz@gmail.com>
2023-11-18 14:09:50 +0100

    idiotproofing resintalll script

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,9 @@ python3 setup.py sdist bdist_wheel
 cd dist
 pip3 uninstall osm_bot_abstraction_layer -y
 pip3 install --user *.whl
-pip3.10 uninstall osm_bot_abstraction_layer -y
-pip3.10 install --user *.whl
+python3.10 -m pip uninstall osm_bot_abstraction_layer -y
+python3.10 -m pip install --user *.whl
+# pip3.10: command not found
 cd ..
 python3 -m unittest
 # twine upload dist/* # to upload to PyPi
