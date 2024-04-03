ee133466f719f3272d1f32108cadf15f4ae0ec1d
Mateusz Konieczny <matkoniecz@gmail.com>
2024-04-03 08:22:14 +0200

    in case json/csv is there it is better to skip it as it will fail anyway

--- osm_bot_abstraction_layer/overpass_downloader.py
+++ osm_bot_abstraction_layer/overpass_downloader.py
@@ -65,7 +65,7 @@ def get_response_from_overpass_server(query, timeout, user_agent):
                 response_length = len(response.content.decode('utf-8'))
                 print("response length:", response_length)
                 # very long indicates that data was returned, some may be some massive that parsing would be outrageously expensive
-                if response_length < 10_000:
+                if response_length < 10_000 and "[out:json]" not in query and "[out:csv]" not in query:
                     parsed = lxml.etree.fromstring(response.content) # yes, without .decode('utf-8')
                     if parsed.find('remark') != None:
                         raise Exception('timeout in query or other failure!' + query)
