543d3fbc816f24082a2bebfb3806cd02fc6a05ff
Mateusz Konieczny <matkoniecz@gmail.com>
2024-04-17 20:34:05 +0200

    provide utility to download files

---
+++ osm_bot_abstraction_layer/util_download_file.py
@@ -0,0 +1,16 @@
+import os
+
+def download_file_if_not_present_already(download_url, directory, filename):
+    filepath = directory + filename
+    if os.path.isdir(directory) == False:
+        print(directory, "is not directory!")
+        raise Exception(directory + " is not directory!")
+    if os.path.isdir(filepath):
+        print(filepath, "is directory!")
+        raise Exception(filepath + " is directory!")
+    if os.path.isfile(filepath) != True:
+        command = 'curl "' + download_url + '" > "' + filepath + '"'
+        print(command)
+        os.system(command)
+    else:
+        print(filepath, "downloaded already")
