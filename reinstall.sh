06d66b2903ea7cfb1f5d2ce08445cf7ec2d39422
Mateusz Konieczny <matkoniecz@gmail.com>
2025-03-11 04:29:49 +0100

    new version after pip ban

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,6 @@
 rm dist -rf
-python3 setup.py sdist bdist_wheel
-cd dist
-/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python ../../python_package_reinstaller/reinstaller.py osm_bot_abstraction_layer # yes, it relies on code on my computer - let me know if anyone else wants to run this script
+/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m build
+python3 ../../python_package_reinstaller/reinstaller.py osm_bot_abstraction_layer # yes, it relies on code on my computer - let me know if anyone else wants to run this script
 cd ..
-/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python -m unittest
+/home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m unittest
 # twine upload dist/* # to upload to PyPi
