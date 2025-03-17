9998cd144cb6a96dcb2899d2cfca7bb6befdc762
Mateusz Konieczny <matkoniecz@gmail.com>
2025-03-17 13:12:48 +0100

    fix modernization of hackish reinstall script

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,5 @@
 rm dist -rf
 /home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m build
 python3 ../../python_package_reinstaller/reinstaller.py osm_bot_abstraction_layer # yes, it relies on code on my computer - let me know if anyone else wants to run this script
-cd ..
 /home/mateusz/Documents/install_moje/shared_python_virtual_environment/bin/python3 -m unittest
 # twine upload dist/* # to upload to PyPi
