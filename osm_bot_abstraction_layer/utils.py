b7be5dc228e8c946c416a4aa163cf204a823d144
Mateusz Konieczny <matkoniecz@gmail.com>
2025-05-14 14:31:36 +0200

    deal with nonbreaking spaces

--- osm_bot_abstraction_layer/utils.py
+++ osm_bot_abstraction_layer/utils.py
@@ -8,14 +8,16 @@ def typical_osm_timestamp_format():
     return "%Y-%m-%dT%H:%M:%SZ"
 
 def tag_in_wikimedia_syntax(key, value):
-    if " " in key and " " in value:
-        return "'" + key + "' = '" + value + "' (note presence of spaces!)"
-    if " " in key:
-        return "'" + key + "' = " + value + " (note presence of spaces!)"
-    if " " in value:
-        return key + " = '" + value + "' (note presence of spaces!)"
-    if "\n" in value:
-        return key + " = '" + value + "' (note presence of newline(s)!)"
+    nonbreaking_space = chr(160)
+    for space in [" ", nonbreaking_space]:
+        if space in key and space in value:
+            return "'" + key + "' = '" + value + "' (note presence of spaces!)"
+        if space in key:
+            return "'" + key + "' = " + value + " (note presence of spaces!)"
+        if space in value:
+            return key + " = '" + value + "' (note presence of spaces!)"
+        if "\n" in value:
+            return key + " = '" + value + "' (note presence of newline(s)!)"
     for problem in ["[", "]", "{", "}", "|"]:
         if problem in key or problem in value:
             return key + "=" + value
