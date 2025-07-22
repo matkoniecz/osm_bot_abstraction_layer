d308d4bd51c335ac1009608b86fbf2ea4afb9396
Mateusz Konieczny <matkoniecz@gmail.com>
2025-07-22 16:01:38 +0200

    fix escaping a bit

--- osm_bot_abstraction_layer/overpass_query_maker.py
+++ osm_bot_abstraction_layer/overpass_query_maker.py
@@ -4,4 +4,8 @@ def datetime_to_overpass_date_format(datetime_data):
     return datetime_data.strftime(utils.typical_osm_timestamp_format())
 
 def escape_overpass_value(value):
+    if "'" in value and '"' in value:
+        raise Exception("both \" and ' in value, simple quoting impossible")
+    if "'" in value:
+        return '"' + value.replace("\\", "\\\\") + '"'
     return "'" + value.replace("\\", "\\\\") + "'"
